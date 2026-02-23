3f516af11abd6316f4f0141cd38f9caa43df0129
connectiondatacenter <connectiondatacenter@gmail.com>
2026-02-24 02:23:08 +0300

    cors fixed

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const { createClient } = require("webdav");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.post("/upload", async (req, res) => {
